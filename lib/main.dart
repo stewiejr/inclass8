3a69b14ec90e6bdfdc2893c7eb7b940fa3032528
Steven Phan <phansteven913@gmail.com>
2025-10-01 22:14:51 -0400

    implemented color picker

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 
 void main() {
   runApp(const MyApp());
@@ -28,34 +29,75 @@ class _MyAppState extends State<MyApp> {
       theme: ThemeData.light(useMaterial3: true),
       darkTheme: ThemeData.dark(useMaterial3: true),
       themeMode: _themeMode,
+      debugShowCheckedModeBanner: false,
       home: MainScreen(onThemeModePressed: _toggleTheme),
     );
   }
 }
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
   final VoidCallback onThemeModePressed;
   const MainScreen({super.key, required this.onThemeModePressed});
 
   @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  Color _textColor = Colors.black;
+
+  void _showColorPicker() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Pick a color'),
+          content: SingleChildScrollView(
+            child: ColorPicker(
+              pickerColor: _textColor,
+              onColorChanged: (Color color) {
+                setState(() => _textColor = color);
+              },
+            ),
+          ),
+          actions: <Widget>[
+            ElevatedButton(
+              child: const Text('Got it'),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
     final isDarkMode = Theme.of(context).brightness == Brightness.dark;
+    if (isDarkMode && _textColor == Colors.black) {
+      _textColor = Colors.white;
+    } else if (!isDarkMode && _textColor == Colors.white) {
+      _textColor = Colors.black;
+    }
 
     return Scaffold(
       appBar: AppBar(
         title: const Text('Flutter Animations'),
         actions: [
           IconButton(
-            icon: Icon(
-                isDarkMode ? Icons.wb_sunny_outlined : Icons.nightlight_round),
-            onPressed: onThemeModePressed,
+            icon: Icon(isDarkMode ? Icons.wb_sunny_outlined : Icons.nightlight_round),
+            onPressed: widget.onThemeModePressed,
+          ),
+          IconButton(
+            icon: const Icon(Icons.color_lens_outlined),
+            onPressed: _showColorPicker,
           ),
         ],
       ),
       body: PageView(
-        children: const [
-          FirstAnimationPage(),
-          SecondAnimationPage(),
+        children: [
+          FirstAnimationPage(textColor: _textColor),
+          SecondAnimationPage(textColor: _textColor),
         ],
       ),
     );
@@ -63,7 +105,8 @@ class MainScreen extends StatelessWidget {
 }
 
 class FirstAnimationPage extends StatefulWidget {
-  const FirstAnimationPage({super.key});
+  final Color textColor;
+  const FirstAnimationPage({super.key, required this.textColor});
 
   @override
   State<FirstAnimationPage> createState() => _FirstAnimationPageState();
@@ -71,6 +114,7 @@ class FirstAnimationPage extends StatefulWidget {
 
 class _FirstAnimationPageState extends State<FirstAnimationPage> {
   bool _isVisible = true;
+  bool _showFrame = false;
 
   void _toggleVisibility() {
     setState(() {
@@ -80,33 +124,64 @@ class _FirstAnimationPageState extends State<FirstAnimationPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          GestureDetector(
-            onTap: _toggleVisibility,
-            child: AnimatedOpacity(
-              opacity: _isVisible ? 1.0 : 0.0,
-              duration: const Duration(seconds: 1),
-              curve: Curves.easeInOut,
-              child: const Text(
-                'Tap Me to Fade!',
-                style: TextStyle(fontSize: 28),
-              ),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        GestureDetector(
+          onTap: _toggleVisibility,
+          child: AnimatedOpacity(
+            opacity: _isVisible ? 1.0 : 0.0,
+            duration: const Duration(seconds: 1),
+            curve: Curves.easeInOut,
+            child: Text(
+              'Tap Me to Fade!',
+              style: TextStyle(fontSize: 28, color: widget.textColor),
             ),
           ),
-          const SizedBox(height: 40),
-          const Text("Swipe for next screen -->", style: TextStyle(fontSize: 16)),
-        ],
-      ),
+        ),
+        Column(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                border: _showFrame
+                    ? Border.all(color: Colors.blueAccent, width: 5)
+                    : null,
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10.0),
+                child: Image.network(
+                  'https://picsum.photos/250?image=9',
+                  width: 200,
+                  height: 200,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text('Show Frame'),
+                Switch(
+                  value: _showFrame,
+                  onChanged: (bool value) {
+                    setState(() => _showFrame = value);
+                  },
+                ),
+              ],
+            ),
+          ],
+        ),
+        const Text("Swipe for next screen -->", style: TextStyle(fontSize: 16)),
+      ],
     );
   }
 }
 
 class SecondAnimationPage extends StatefulWidget {
-  const SecondAnimationPage({super.key});
-
+  final Color textColor;
+  const SecondAnimationPage({super.key, required this.textColor});
   @override
   State<SecondAnimationPage> createState() => _SecondAnimationPageState();
 }
@@ -119,7 +194,6 @@ class _SecondAnimationPageState extends State<SecondAnimationPage> {
       _isVisible = !_isVisible;
     });
   }
-
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -132,9 +206,9 @@ class _SecondAnimationPageState extends State<SecondAnimationPage> {
               opacity: _isVisible ? 1.0 : 0.0,
               duration: const Duration(milliseconds: 3000),
               curve: Curves.fastLinearToSlowEaseIn,
-              child: const Text(
+              child: Text(
                 'Slower Fade!',
-                style: TextStyle(fontSize: 28),
+                style: TextStyle(fontSize: 28, color: widget.textColor),
               ),
             ),
           ),
